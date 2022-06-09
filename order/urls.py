d7bb1898a07b1ce66479efee8cdd4921635eb298
유유카 <ucar@yuyukaui-MacBookPro.local>
2022-06-10 08:42:21 +0900

    CH02_12 (백앤드) Order 백엔드 개발2

--- order/urls.py
+++ order/urls.py
@@ -19,6 +19,7 @@ from order import views
 urlpatterns = [
     path('shops/',views.shop, name ="shop"),
     path('menus/<int:shop>',views.menu, name ="menu"),
+    path('order/', views.order, name ='order')
     
 
 ]
--- order/views.py
+++ order/views.py
@@ -33,7 +33,7 @@ def menu(request, shop):
         menu = Menu.objects.filter(shop=shop)
         # serializer = MenuSerializer(menu, many=True)
         # return JsonResponse(serializer.data, safe=False)
-        return render(request, 'order/menu_list.html',{'menu_list':menu})
+        return render(request, 'order/menu_list.html',{'menu_list':menu, 'shop' : shop})
     
     elif request.method =="POST":
         data = JSONParser().parse(request)
@@ -42,3 +42,26 @@ def menu(request, shop):
             serializer.save()
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
+
+
+from django.utils import timezone
+@csrf_exempt
+def order(request):
+    if request.method == 'POST':
+        address = request.POST['address']
+        shop =request.POST['shop']
+        food_list = request.POST.getlist('menu')
+        order_date = timezone.now()
+
+        shop_item = Shop.objects.get(pk=shop)
+        #order 테이블에 외래키를 가지는 항목을 만들고 싶다.
+        shop_item.order_set.create(address = address, order_date = order_date, shop=int(shop)) 
+
+        order_item = Order.objects.get(pk=shop_item.order_set.latest('id').id)
+        for food in food_list:
+            order_item.orderfood_set.create(food_name = food)
+        return HttpResponse(status = 200)
+
+    elif request.method =='GET':
+        order_list = Order.objects.all()
+        return render(request, 'order/order_list.html', {'order_list' : order_list})
