e3ced2c0c70a0d8c4abc041f9383c76a0453a067
유유카 <ucar@yuyukaui-MacBookPro.local>
2022-06-09 15:59:01 +0900

    CH02_11 (백앤드) Order 백엔드 개발2

---
+++ order/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+
+from order.models import Shop, Menu, Order, Orderfood
+from order.serializers import ShopSerializer, MenuSerializer
+
+
+# insomnia
+# Create your views here.
+@csrf_exempt
+def shop(request):
+    if request.method == "GET":
+        # shop = Shop.objects.all()
+        # serializer = ShopSerializer(shop, many=True)
+        # return JsonResponse(serializer.data, safe=False)
+        shop =Shop.objects.all()
+        return render(request, 'order/shop_list.html',{'shop_list':shop})
+        # return render(request, 'order/shop_list/html')
+    elif request.method =="POST":
+        data = JSONParser().parse(request)
+        serializer = ShopSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def menu(request, shop):
+    if request.method == "GET":
+        menu = Menu.objects.filter(shop=shop)
+        # serializer = MenuSerializer(menu, many=True)
+        # return JsonResponse(serializer.data, safe=False)
+        return render(request, 'order/menu_list.html',{'menu_list':menu})
+    
+    elif request.method =="POST":
+        data = JSONParser().parse(request)
+        serializer = MenuSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
